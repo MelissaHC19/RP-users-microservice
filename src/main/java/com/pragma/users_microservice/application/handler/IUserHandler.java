cb8105a340a466669053a04943948fc95b178761
Melissa Henao Cardona <melissa.henao@utp.edu.co>
2024-10-18 16:59:07 -0500

    refactor: removed unused import statements in IUserHandler and IRegisterUserRequestMapper

--- src/main/java/com/pragma/users_microservice/application/handler/IUserHandler.java
+++ src/main/java/com/pragma/users_microservice/application/handler/IUserHandler.java
@@ -2,7 +2,6 @@ package com.pragma.users_microservice.application.handler;
 
 import com.pragma.users_microservice.application.dto.request.RegisterUserRequest;
 import com.pragma.users_microservice.application.dto.response.GetUserResponse;
-import com.pragma.users_microservice.domain.model.User;
 
 public interface IUserHandler {
     void createOwner(RegisterUserRequest registerUserRequest);
--- src/main/java/com/pragma/users_microservice/application/mapper/IRegisterUserRequestMapper.java
+++ src/main/java/com/pragma/users_microservice/application/mapper/IRegisterUserRequestMapper.java
@@ -1,11 +1,9 @@
 package com.pragma.users_microservice.application.mapper;
 
 import com.pragma.users_microservice.application.dto.request.RegisterUserRequest;
-import com.pragma.users_microservice.domain.model.Role;
 import com.pragma.users_microservice.domain.model.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface IRegisterUserRequestMapper {
